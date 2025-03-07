9d2ebd78ee7af84b130abb7ecb86669a60dedba8
jukramer <justas.kramer@gmail.com>
2025-03-07 19:26:32 +0100

    Updated

--- Terrain-Generator/main.py
+++ Terrain-Generator/main.py
@@ -1,21 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from noise import snoise2 as sn
+import random
 
 # Parameters
-width = 100
-height = 100
+mapSize = (1000, 1000)
+scale = 200
 
-def generateTerrain():
-    terrain = np.zeros((height, width))
+def generateTerrain(mapSize):
+    terrain = np.zeros(mapSize)
+    seed = random.randint(0, 1000)
 
-    for y in range(height):
-        for x in range(width):
-            terrain[y,x]=sn(x, y, octaves=4, persistence=0.5, lacunarity=2.0)
+    for y in range(mapSize[1]):
+        for x in range(mapSize[0]):
+            terrain[y,x]=sn(x/scale, y/scale, octaves=3, persistence=0.5, lacunarity=2.0, base=seed)
 
     return terrain
 
+def normalize(matrix):
+    a = np.min(matrix)
+    b = np.max(matrix)
+
+    if a == b:
+        print('Matrix may not have the same values everywhere.')
+        raise ZeroDivisionError
+
+    return matrix*a/(b-a) + a/(a-b)
+
 if __name__ == '__main__':
-    plt.imshow(generateTerrain(), cmap='terrain')
+    plt.imshow(normalize(generateTerrain(mapSize)), cmap='terrain')
     plt.colorbar()
     plt.show() 
+
+
