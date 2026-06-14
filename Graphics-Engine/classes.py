36073829bb48dcc89d39cbc139313ee447bca9b9
jukramer <justas.kramer@gmail.com>
2026-06-14 18:16:46 +0200

    updates

--- Graphics-Engine/classes.py
+++ Graphics-Engine/classes.py
@@ -1,3 +1,4 @@
+import colorsys as cls
 import itertools as it
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,6 +17,7 @@ RZ = lambda x: np.array([[np.cos(x), -np.sin(x), 0],
                          [0, 0, 1]])
 
 FRAMERATE = 60
+xVals = np.zeros((8,2,1))
 
 # =========================== CLASSES ============================
 # ============== GEOMETRY
@@ -79,11 +81,12 @@ class Cube(Object):
 
 # ============== RENDERING
 class Cam:
-    def __init__(self, pos: np.ndarray, target: np.ndarray, fov: float | int, near: float, up=np.array([0,1,0])):
+    def __init__(self, pos: np.ndarray, target: np.ndarray, up, fov: float | int, near: float, far: float):
         self.pos = pos
         self.target = np.array(target)/np.linalg.norm(target)
         self.fov = fov
         self.near = near
+        self.far = far
         self.up = np.array(up)
         self.right = np.cross(self.target, self.up)
             
@@ -97,21 +100,18 @@ class Render:
         self.clock = pg.time.Clock()
         self.res = res
         self.cam = cam
-              
-    def renderWireframe(self, obj: Object):
-        X = self.findProjection(obj)[:,:2] 
-        
-        # Scale X to screen
-        XScaled = min(self.res)*0.4*X
-        XScaled[:,0] += self.res[0]/2
-        XScaled[:,1] += self.res[1]/2
-        print(XScaled)
-        
+           
+    # ============= Rendering   
+    def render(self, objs: list):
         pg.init()
+        self.renderWireframe(*objs, rotation=(0, 0, np.pi/6))
         running = True
-        theta = np.pi/800
+        theta = 0
         while running:
             self.clock.tick(FRAMERATE)
+            theta += np.pi/600
+            
+            # Inputs/Events
             keys = pg.key.get_pressed()
             for event in pg.event.get():
                 if event.type == pg.QUIT:
@@ -126,52 +126,134 @@ class Render:
                 theta = np.pi/800
             if keys[pg.K_x]:
                 theta = -np.pi/800
+            # Draw Scene   
+            self.scr.fill("#FFFFFF")
+            for obj in objs:
+                self.renderWireframe(obj, rotation=(0, 0, theta))
             
-            self.scr.fill('#FFFFFF')
-            
-            # Spin cube
-            X = self.findProjection(obj)[:,:2] 
-        
-            # Scale X to screen 
-            XScaled = min(self.res)*0.4*X
-            XScaled[:,0] += self.res[0]/2
-            XScaled[:,1] += self.res[1]/2
-            obj.vertices = obj.vertices @ RX(theta).T
-            # obj.vertices = obj.vertices @ RX(theta).T
-            
-            # Draw lines
-                
-            # Draw vertices
-            for vert in list(XScaled):
-                pg.draw.circle(self.scr, '#FF0000', vert, 5)
-                
             pg.display.flip()
+        
+        xVals = xVals.transpose((0,2,1))
+        tVals = np.linspace(0, 1, xVals.shape[1])
+        plt.plot(tVals, xVals[:,])
+        plt.show()
+            
+    def renderWireframe(self, obj: Object, scaling=np.ones(3), rotation=np.zeros(3), translation=np.zeros(3)):
+        X = self.findProjection(obj, scaling, rotation, translation)[:,:2] 
+        global xVals
+    
+        # Scale X to screen 
+        XScaled = min(self.res)*0.4*X
+        XScaled[:,0] += self.res[0]/2
+        XScaled[:,1] += self.res[1]/2
+        # obj.vertices = obj.vertices @ RX(theta).T
+        # obj.vertices = obj.vertices @ RX(theta).T
+        
+        # Draw lines
+        for face in list(obj.faces):
+            for line in list(it.combinations(face, 2)):
+                pg.draw.line(self.scr, '#000000', XScaled[line[0],:], XScaled[line[1],:])
+                
+        # Draw vertices
+        for i, vert in enumerate(list(XScaled)):
+            if i < 4:
+                color = '#FF0000'
+            else: 
+                color = '#00AAFF'
+            xVals = np.dstack([xVals, XScaled])
+            pg.draw.circle(self.scr, color, vert, 5)
 
+    def findProjection(self, obj: Object, scaling: np.ndarray, rotation: np.ndarray, translation: np.ndarray):
+        if len(scaling) not in {3, (3,), (3,1)}:
+            raise ValueError('Scaling array must have 3 entries.')
+        if len(rotation) not in {3, (3,), (3,1)}:
+            raise ValueError('Rotation array must have 3 entries.')
+        if len(translation) not in {3, (3,), (3,1)}:
+            raise ValueError('Translation array must have 3 entries.')
         
-    def findProjection(self, obj: Object):
+        # Model Matrix
+        S = np.array([[scaling[0], 0, 0, 0],
+                      [0, scaling[1], 0, 0],
+                      [0, 0, scaling[2], 0],
+                      [0, 0, 0, 1]])
+        
+        RX = lambda x: np.array([[1, 0, 0, 0],
+                                 [0, np.cos(x), -np.sin(x), 0],
+                                 [0, np.sin(x), np.cos(x), 0],
+                                 [0, 0, 0, 1]])
+        
+        RY = lambda x: np.array([[np.cos(x), 0, np.sin(x), 0],
+                                 [0, 1, 0, 0],
+                                 [-np.sin(x), 0, np.cos(x), 0],
+                                 [0, 0, 0, 1]])
+        
+        RZ = lambda x: np.array([[np.cos(x), -np.sin(x), 0, 0],
+                                 [np.sin(x), np.cos(x), 0, 0],
+                                 [0, 0, 1, 0],
+                                 [0, 0, 0, 1]])
+        
+        R = RZ(rotation[2]) @ RY(rotation[1]) @ RX(rotation[0])
+        
+        T = np.array([[1, 0, 0, translation[0]],
+                      [0, 1, 0, translation[1]],
+                      [0, 0, 1, translation[2]],
+                      [0, 0, 0, 1]])
+        
+        M = T @ R @ S
+        
+        # View Matrix
+        V = np.array([[*self.cam.right, -self.cam.right@self.cam.pos],
+                      [*self.cam.up, -self.cam.up@self.cam.pos],
+                      [*(-self.cam.target), self.cam.target@self.cam.pos],
+                      [0, 0, 0, 1]])
+        
+        # Projection Matrix
         X = np.hstack([obj.vertices, np.ones((obj.vertices.shape[0], 1))])
-        P = np.array([[self.cam.near, 0, 0, 0],
-                      [0, self.cam.near, 0, 0],
-                      [0, 0, 1, 0],
-                      [0, 0, 1, 0]])
-        XProj = X @ P.T
+        a = 1 # aspect ratio
+        P = np.array([[1/(a*np.tan(self.cam.fov/2)), 0, 0, 0],
+                      [0, 1/np.tan(self.cam.fov/2), 0, 0], 
+                      [0, 0, -(self.cam.far+self.cam.near)/(self.cam.far-self.cam.near), -2*self.cam.far*self.cam.near/(self.cam.far-self.cam.near)],
+                      [0, 0, -1, 0]])
         
-        return XProj[:,:3]/np.tile(XProj[:,3], (3,1)).T
+        XHom = np.hstack([obj.vertices, np.ones_like(obj.vertices[:,0])[:,None]])
+        XWorld = XHom @ M.T
+        XCam = XWorld @ V.T
+        XClip = XCam @ P.T
+        
+        return XClip[:,:3]/np.tile(XClip[:,3], (3,1)).T
 
 # ========================= FUNCTIONS ==========================
 def main():
-    # cam = Cam(np.array([0, 0, 0]), [0, 1, 0], 30, 1, up = [0, 0, 1])
-    # obj1 = Object(np.array([[2, 2, 2],
+    cam = Cam(np.array([2, 0, 4]), [0, 0, -1], [0, 1, 0], 90, 1, 50)
+    # obj1 = Object(0.5*np.array([[2, 2, 2],
     #                         [2, -2, 2],
     #                         [-2, 2, 2],
     #                         [-2, -2, 2],
     #                         [2, 2, 4],
     #                         [2, -2, 4],
     #                         [-2, 2, 4],
-    #                         [-2, -2, 4],]))
-    # render = Render((1000, 800), cam)
-    # render.renderWireframe(obj1)
-    cube = Cube(np.array(list(it.product([1,-1], repeat=3))))
+    #                         [-2, -2, 4]]),
+    #               np.array([]))
+    render = Render((1000, 800), cam)
+    cube1 = Cube(np.array([[1,1,1],
+                        [1, -1, 1],
+                        [-1,1, 3],
+                        [-1,-1,1],
+                        [-1,1,1],
+                        [1,1, 3],
+                        [1,-1, 3],
+                        [-1,-1,3]]))
+    
+    cube2 = Cube(np.array([[2, 2, 6],
+                            [2, -2, 6],
+                            [-2, 2, 6],
+                            [-2, -2, 6],
+                            [2, 2, 2],
+                            [2, -2, 2],
+                            [-2, 2, 2],
+                            [-2, -2, 2]]))
+    
+    render.render([cube2])
     
 
 if __name__ == '__main__':
